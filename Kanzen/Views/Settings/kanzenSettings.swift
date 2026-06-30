7fb1d6831f936226b2658a71c8e1ca9289d716fb
cranci1 <100066266+cranci1@users.noreply.github.com>
2026-06-30 10:53:31 +0200

    Replaced "Sora" with "Luna"

--- Kanzen/Views/Settings/kanzenSettings.swift
+++ Kanzen/Views/Settings/kanzenSettings.swift
@@ -33,7 +33,7 @@ struct KanzenSettingsView : View
                     
                 }
                 Section(header: Text("Others")){
-                    Text("Switch to Sora")
+                    Text("Switch to Luna")
                         .onTapGesture {
                             showKanzen = false
                         }
